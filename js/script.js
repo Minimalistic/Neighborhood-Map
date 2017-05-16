6b0bad1d3d6db8bfce743e650bc53c5c3de62e03
Jason Marsh <regularly.jason@gmail.com>
2017-05-16 14:01:19 -0500

    Adjusted code to fit in 80 column

--- js/script.js
+++ js/script.js
@@ -18,7 +18,10 @@ function loadData() {
     $greeting.text('So, you want to live at ' + address + '?');
 
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    var streetviewUrl = 
+        'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' 
+            + address 
+            + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
@@ -26,7 +29,10 @@ function loadData() {
 
     // load nytimes
     // obviously, replace all the "X"s with your own API key
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=2318ea7506f24fe1ab63d696010800a2'
+    var nytimesUrl = 
+        'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' 
+            + cityStr 
+            + '&sort=newest&api-key=2318ea7506f24fe1ab63d696010800a2'
     $.getJSON(nytimesUrl, function(data){
 
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
@@ -47,7 +53,10 @@ function loadData() {
 
 
     // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiUrl = 
+        'http://en.wikipedia.org/w/api.php?action=opensearch&search=' 
+            + cityStr 
+            + '&format=json&callback=wikiCallback';
     var wikiRequestTimeout = setTimeout(function(){
         $wikiElem.text("failed to get wikipedia resources");
     }, 8000);
@@ -62,7 +71,10 @@ function loadData() {
             for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+                $wikiElem.append('<li><a href="' 
+                    + url + '">' 
+                    + articleStr 
+                    + '</a></li>');
             };
 
             clearTimeout(wikiRequestTimeout);
